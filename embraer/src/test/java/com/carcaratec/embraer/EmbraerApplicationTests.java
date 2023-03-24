73823ed3419d78a942e9f7506c4b4a47d64cd809
Alan Araujo <alanaraujotrs@gmail.com>
2023-03-24 19:27:30 -0300

    teste

---
+++ embraer/src/test/java/com/carcaratec/embraer/EmbraerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carcaratec.embraer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmbraerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
