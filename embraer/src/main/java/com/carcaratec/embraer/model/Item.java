4c934a4b73240a5a3ee757cf03933d6a4c9bab99
Igor Suzuki Kira <89170220+igorsuzuki99@users.noreply.github.com>
2023-03-28 10:41:12 -0300

    Create class Item

---
+++ embraer/src/main/java/com/carcaratec/embraer/model/Item.java
@@ -0,0 +1,43 @@
+package com.carcaratec.embraer.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "ITEM")
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "ID_ITEM", nullable = false)
+    private Integer idItem;
+
+    @Column(name = "NOME")
+    private String nome;
+
+    @Column(name = "DESCRICAO")
+    private String descricao;
+
+    public Integer getIdItem() {
+        return idItem;
+    }
+
+    public void setIdItem(Integer idItem) {
+        this.idItem = idItem;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
