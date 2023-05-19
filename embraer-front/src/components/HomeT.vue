1eb7d71e3ae6228d5b8301a99ab6ed74f0191eb0
Wesley Ferreira da Silva <ferreiras.wesley@gmail.com>
2023-05-18 21:52:19 -0300

    "começo da refatoração do front. html para vue"

---
+++ embraer-front/src/components/HomeT.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="logo-container p-3 d-flex justify-content-between">
+    <span></span>
+    <img src="../assets/logo.embraer.png" />
+    <div>
+      <i class="fa-solid fa-bell"></i>
+      <i class="fa-solid fa-user"></i>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+ export default defineComponent({
+    name: 'HomeT'
+})
+</script>
+
+<style>
+.logo-container {
+  background-color: rgb(11, 27, 91);
+  color: #fff;
+}
+
+.btn-group button {
+  background-color: rgb(11, 27, 91);
+  margin-right: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.fa-solid {
+  margin-right: 20px;
+  margin-left: 20px;
+}
+</style>
